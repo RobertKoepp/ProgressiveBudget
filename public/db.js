0caff598f89000b2240833d617d134efc4f5e9b7
Robert Koepp <rkoepp2@gmail.com>
2021-02-25 18:31:56 -0800

    created db.js file

--- public/db.js
+++ public/db.js
@@ -5,3 +5,21 @@ const indexedDB =
   window.msIndexedDB ||
   window.shimIndexedDB;
 let db;
+var request = indexedDB.open("budgetapp",1)
+request.onupgradeneeded = function({target}) {
+    const db = target.result;
+    db.createObjectStore("working",{
+        autoIncrement: true
+    });
+  };
+request.onsuccess = function({target}){
+    db = target.result
+    if(navigator.onLine){
+        lookDatabase()
+    }
+}
+function saveDB(opject){
+    const transaction = db.transaction(["working"],"readwrite")
+const objectsStore=transaction.objectsStore("working")
+objectsStore.add(object)
+}
