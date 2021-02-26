a2c32fd61de3d9ad1f40a1efaf1ba2dfe4219027
Robert Koepp <rkoepp2@gmail.com>
2021-02-25 18:36:36 -0800

    db.js additions

--- public/db.js
+++ public/db.js
@@ -23,3 +23,30 @@ function saveDB(opject){
 const objectsStore=transaction.objectsStore("working")
 objectsStore.add(object)
 }
+function lookDatabase(){
+    const budget = db.transaction(["pending"], "readwrite");
+  const objectstore = budget.objectStore("pending");
+  const getAll = objectstore.getAll();
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(() => {
+        // delete records if successful
+        const budget = db.transaction(["working"], "readwrite");
+        const objectstore = budget.objectStore("working");
+        objectstore.clear();
+      });
+    }
+  };
+}
+window.addEventListener("working", lookDatabase)
